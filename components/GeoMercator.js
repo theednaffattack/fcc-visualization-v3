499aa3a859c977ee5f19bb1131a624f911add201
theednaffattack <eddienaff@gmail.com>
2018-04-07 09:20:40 -0700

    Prepare to sync from origin

--- components/GeoMercator.js
+++ components/GeoMercator.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { GradientTealBlue, RadialGradient } from '@vx/gradient';
-import { Mercator } from '@vx/geo';
-import * as topojson from 'topojson-client';
-import topology from '../static/world-topo.json';
-import meteorStrikes from '../static/meteor-strike-data.json';
+import React from "react";
+import { GradientTealBlue, RadialGradient } from "@vx/gradient";
+import { Mercator } from "@vx/geo";
+import { genRandomNormalPoints } from "@vx/mock-data";
+import { GlyphCircle } from "@vx/glyph";
+import * as topojson from "topojson-client";
+import topology from "../static/world-topo.json";
+import meteorStrikes from "../static/meteor-strike-data.json";
+
+const points = genRandomNormalPoints(600).filter((d, i) => {
+  return i < 600;
+});
 
 export default ({ width, height, events = false }) => {
   if (width < 10) return <div />;
 
   const world = topojson.feature(topology, topology.objects.units);
 
-//   const meteorData = topojson.feature(meteorStrikes, meteorst)
+  // const meteorData = topojson.feature(meteorStrikes, meteorStrikes.foo);
 
-  console.log(world)
+  console.log(JSON.stringify(meteorStrikes, null, 2));
+  // console.log(JSON.stringify(topology, null, 2));
 
   return (
     <svg width={width} height={height}>
@@ -20,7 +27,7 @@ export default ({ width, height, events = false }) => {
         id="geo_mercator_radial"
         from="#55bdd5"
         to="#4f3681"
-        r={'80%'}
+        r={"80%"}
       />
       <rect
         x={0}
@@ -34,8 +41,8 @@ export default ({ width, height, events = false }) => {
         data={world.features}
         scale={width / 630 * 100}
         translate={[width / 2, height / 2 + 50]}
-        fill={() => '#8be4c5'}
-        stroke={() => '#5fcfa7'}
+        fill={() => "#8be4c5"}
+        stroke={() => "#5fcfa7"}
         onClick={data => event => {
           if (!events) return;
           alert(`Clicked: ${data.properties.name} (${data.id})`);
